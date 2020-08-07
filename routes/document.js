0f68b907ad9a8e1ee680a63963a54a6ff8a9f391
Rahul Ranjan <rahul.smile@gmail.com>
2020-08-08 01:27:03 +0530

    Multi file support

--- routes/document.js
+++ routes/document.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const path = require('path');
 
 var documents = [
-  { id:"1", name: "Toyota", description: "Celica", uploadDate: 1596385482000, isUploadedProgress: 100, previewUrl: 'http://www.linkedin.com', type: '.pdf' },
-  { id:"2",name: "Ford", description: "Mondeo", uploadDate: 1596385482000, isUploadedProgress: 33, previewUrl: 'http://www.yahoo.com', type: '.doc' },
-  { id:"3",name: "Porsche", description: "Boxter", uploadDate: 1596385482000, isUploadedProgress: 100, previewUrl: 'http://www.google.com', type: '.xlsx' },
-  { id:"4",name: "Porsche", description: "Boxter", uploadDate: 1596385482000, isUploadedProgress: 100, previewUrl: 'http://www.google.com', type: '.xlsx' }
+  { id:"1", name: "Toyota", description: "Celica", uploadDate: 1596385482000, isUploadedProgress: 100, previewUrl: ['http://www.linkedin.com','http://www.linkedin.com','http://www.linkedin.com']},
+  { id:"2",name: "Ford", description: "Mondeo", uploadDate: 1596385482000, isUploadedProgress: 33, previewUrl: ['http://www.yahoo.com']},
+  { id:"3",name: "Porsche", description: "Boxter", uploadDate: 1596385482000, isUploadedProgress: 100, previewUrl: ['http://www.google.com']},
+  { id:"4",name: "Porsche", description: "Boxter", uploadDate: 1596385482000, isUploadedProgress: 100, previewUrl: ['http://www.google.com']}
 ]
 
 /* GET users listing. */
@@ -18,26 +18,46 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   const {name, description, uploadedDate} = req.body
-  let sampleFile = req.files.fileName;
-  const fileExtension = sampleFile.name.split('.').pop().toLowerCase()
-  const fileName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + '.' + fileExtension;
-  sampleFile.mv(`${path.join(__dirname, 'files')}/${fileName}`, function(err) {
-    if (err) return res.status(500).send(err);
-    const uploadedDocument = {
-      id: Math.floor(Math.random()*90000) + 10000,
-      name: name,
-      description: description,
-      uploadDate: uploadedDate,
-      isUploadedProgress: 100,
-      previewUrl: `document/download?filename=${fileName}`,
-      type: fileExtension
-    }
-    documents.push(uploadedDocument)
-    res.json({
-      documents: documents
-    });
-  });
+      , filesKeys = Object.keys(req.files)
+  
+  let ticker = filesKeys.length,
+      uploadedDocument;
+  
+  const uploadFiles = () => {
+    const sampleFile = req.files[filesKeys[ticker-1]]
+        , fileExtension = sampleFile.name.split('.').pop().toLowerCase()
+        , fileName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + '.' + fileExtension;
+    
+    sampleFile.mv(`${path.join(__dirname, 'files')}/${fileName}`, function(err) {
+      if (err) return res.status(500).send(err);
+      
+      if(ticker === filesKeys.length){
+        uploadedDocument = {
+          id: Math.floor(Math.random()*90000) + 10000,
+          name: name,
+          description: description,
+          uploadDate: uploadedDate,
+          isUploadedProgress: 100,
+          previewUrl: [`document/download?filename=${fileName}`],
+          type: fileExtension
+        }
+      }else{
+        uploadedDocument.previewUrl.push(`document/download?filename=${fileName}`)
+      }
+      
+      if(ticker === 1){
+        documents.push(uploadedDocument)
+        res.json({
+          documents: documents
+        });
+      }else{
+        ticker -= 1
+        uploadFiles()
+      }
 
+    });
+  }
+  uploadFiles()
 });
 
 router.get('/download', function(req, res, next) {
